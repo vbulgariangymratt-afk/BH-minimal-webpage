67d0afa9a66deb98df51dccf3de35223d80e73ec
Maximiliano Sors Garza <vbulgariangymratt@gmail.com>
2026-09-01 18:11:39 -0600

    clean: remove root index.html and deprecated devIndicators

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  devIndicators: {
-    appIsrStatus: false,
-    buildActivity: false,
-  },
 };
 
 module.exports = nextConfig;
